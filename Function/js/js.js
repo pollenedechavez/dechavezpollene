551f3311f98725595e6bf065f08272c8007f5e53
pollenedechavez <pollenejoydechavez681@gmail.com>
2024-05-21 16:28:25 +0800

    Add files via upload

---
+++ Function/js/js.js
@@ -0,0 +1,13 @@
+function calc1(num1,num2){
+    return Number(num1)+Number(num2);
+  }
+  function calc2(num1,num2){
+    return Number(num1)-Number(num2);
+  }
+  function calc3(num1,num2){
+    return Number(num1)*Number(num2);
+  } 
+ function calc4(num1,num2){
+    return Number(num1)/Number(num2);
+  }
+  
